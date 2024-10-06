304e28c98849e6386e4445722ac89fa55706133e
Alive Wells <alivewel@mail.ru>
2024-10-06 18:20:54 +0300

    clean trash

--- src/graphs/cmd/main.go
+++ src/graphs/cmd/main.go
@@ -8,31 +8,8 @@ import (
 
 
 func main() {
-	// matrix := [][]int{
-	// 	{0, 14, 7, 16, 17, 9},
-	// 	{14, 0, 12, 12, 11, 9},
-	// 	{7, 12, 0, 19, 19, 12},
-	// 	{16, 12, 19, 0, 2, 8},
-	// 	{17, 11, 19, 2, 0, 9},
-	// 	{9, 9, 12, 8, 9, 0},
-	// }
-	// graph := graph.NewGraph(len(matrix))
-
-	// for i := 0; i < len(matrix); i++ {
-	// 	for j := 0; j < len(matrix[i]); j++ {
-	// 		graph.AddEdge(i, j, matrix[i][j])
-	// 	}
-	// }
-
-	// res := graph_algorithms.SolveTravelingSalesmanProblem(graph)
-	// fmt.Println(res)
-
 	vertexCount := 4
 	g := graph.NewGraph(vertexCount)
-
-	// edges := [][3]int{
-	// 	{0, 1, 10}, {0, 2, 6}, {0, 3, 5}, {1, 3, 15}, {2, 3, 4},
-	// }
 
 	edges := [][3]int{
 		{0, 1, 10}, {0, 2, 6}, {0, 3, 5}, {1, 3, 15}, {2, 3, 4},
